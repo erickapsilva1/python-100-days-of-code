f3649526252d64b7bf595d78d10b51de8189cf46
Erick Silva <erick.ap.silva@gmail.com>
2022-10-03 08:18:45 -0300

    love calculator logic implementation

--- day-3/love-calculator.py
+++ day-3/love-calculator.py
@@ -2,4 +2,30 @@ print('Welcome to the Love Calculator!')
 name1 = input('What is your name? \n')
 name2 = input('What is their name? \n')
 
-name1.lower().count('t')
+combined_string = name1.lower() + name2.lower()
+
+t = combined_string.count('t')
+r = combined_string.count('r')
+u = combined_string.count('u')
+e = combined_string.count('e')
+
+true = t + r + u + e
+
+l = combined_string.count('l')
+o = combined_string.count('o')
+v = combined_string.count('v')
+e = combined_string.count('e')
+
+love = l + o + v + e
+
+score_str = str(true) + str(love)
+
+score = int(score_str)
+
+if score < 10 or score > 90:
+    print(f'Your score is {score}, you go together like coke and mentos.')
+elif score >= 40 or score <= 50:
+    print(f'Your score is {score}, you are alright together.')
+else:
+    print(f'Your score is {score}.')
+
