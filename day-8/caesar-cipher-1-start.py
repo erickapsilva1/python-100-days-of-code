eef2aed108c767fc833c099f9d86cc624aff5a9f
Erick Silva <erick.ap.silva@gmail.com>
2022-10-15 18:21:56 -0300

    adding minor improvement

--- day-8/caesar-cipher-1-start.py
+++ day-8/caesar-cipher-1-start.py
@@ -4,45 +4,32 @@ direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encrypty(text, shift):
+def caesar(text, shift, direction):
     cipher_text = ''
 
     for l in text:
         letter_index = alphabet.index(l)
         
-        new_letter = letter_index + shift
+        if direction == 'encode':
+            new_letter = letter_index + shift
+        elif direction == 'decode':
+            new_letter = letter_index - shift
         
         if new_letter > 25:
-            letter_position = new_letter - len(alphabet)
+            if direction == 'encode':
+                letter_position = new_letter - len(alphabet)
+            elif direction == 'decode':
+                letter_position = new_letter + len(alphabet)
             new_letter = alphabet[letter_position]
         else:
-            new_letter = alphabet[letter_index + shift]
+            if direction == 'encode':
+                new_letter = alphabet[letter_index + shift]
+            elif direction == 'decode':
+                new_letter = alphabet[letter_index - shift]
         
         cipher_text += new_letter
     
-    print(cipher_text)
+    print(f'The {direction}d is {cipher_text}')
 
 
-def decrypt(text, shift):
-    cipher_text = ''
-
-    for l in text:
-        letter_index = alphabet.index(l)
-
-        new_letter = letter_index - shift
-
-        if new_letter > 25:
-            letter_position = new_letter - len(alphabet)
-            new_letter = alphabet[letter_position]
-        else:
-            new_letter = alphabet[letter_index - shift]
-
-        cipher_text += new_letter
-
-    print(cipher_text)
-
-if direction == 'encode':
-    encrypty(text, shift)
-elif direction == 'decode':
-    decrypt(text, shift)
-
+caesar(text, shift, direction)
