bffc1cf9b06b049674d96d44afd9f2457fbe370e
Erick Silva <erick.ap.silva@gmail.com>
2022-10-15 16:59:26 -0300

    adding caesar-cipher-1-start

---
+++ day-8/caesar-cipher-1-start.py
@@ -0,0 +1,25 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypty(text, shift):
+    cipher_text = ''
+
+    for l in text:
+        letter_index = alphabet.index(l)
+        
+        new_letter = letter_index + shift
+
+        if new_letter > 25:
+            letter_position = len(alphabet) - letter_index
+            new_letter = alphabet[letter_position]
+        else:
+            new_letter = alphabet[letter_index + shift]
+        
+        cipher_text += new_letter
+    
+    print(cipher_text)
+
+encrypty(text, shift)
