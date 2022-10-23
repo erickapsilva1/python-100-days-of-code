485c0eac4fb1a7124a714d3cc917328b728a64da
Erick Silva <erick.ap.silva@gmail.com>
2022-10-23 20:20:59 -0300

    adding first version of the calculator using function

---
+++ day-10/calculator-start.py
@@ -0,0 +1,34 @@
+
+# Calculator
+
+def add(n1, n2):
+    return n1 + n2
+
+def subtract(n1, n2):
+    return n1 - n2
+
+def multiply(n1, n2):
+    return n1 * n2
+
+def divide(n1, n2):
+    if n2 > 0:
+        return n1 / n2
+    else:
+        return 'Infinity'
+
+
+operations = {'+': add, '-': subtract, '*': multiply, '/': divide}
+
+num1 = int(input("What's the first number?: "))
+num2 = int(input("What's the second number?: "))
+
+for operation in operations:
+    print(operation)
+
+operation_symbol = input('Pick an operation from the live above: ')
+
+function = operations[operation_symbol]
+
+answer = function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
