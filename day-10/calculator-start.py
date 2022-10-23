57c7e8e5c40b1ce0c3b1317e108b3d15dfe848d1
Erick Silva <erick.ap.silva@gmail.com>
2022-10-23 20:46:12 -0300

    adding calculator improvement (loop)

--- day-10/calculator-start.py
+++ day-10/calculator-start.py
@@ -20,15 +20,31 @@ def divide(n1, n2):
 operations = {'+': add, '-': subtract, '*': multiply, '/': divide}
 
 num1 = int(input("What's the first number?: "))
-num2 = int(input("What's the second number?: "))
 
 for operation in operations:
     print(operation)
 
-operation_symbol = input('Pick an operation from the live above: ')
+operation_symbol = input('Pick an operation: ')
+num2 = int(input("What's the second number?: "))
 
 function = operations[operation_symbol]
-
 answer = function(num1, num2)
 
 print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+should_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit. ").lower()
+
+while should_continue:
+
+    if should_continue == 'y':
+        operation_symbol = input('Pick an operation: ')
+        next_number = int(input("What's the next number?: "))
+        function = operations[operation_symbol]
+        old_answer = answer
+        answer = function(answer, next_number)
+        print(f"{old_answer} {operation_symbol} {next_number} = {answer}")
+
+        should_continue = input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit. ").lower()
+    else:
+        break
+
