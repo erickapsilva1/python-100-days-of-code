6d22bd69ed79c620ac04eabcae83a2a25c588798
Erick Silva <erick.ap.silva@gmail.com>
2022-11-22 07:14:29 -0300

    gui - drawing a different shapes - adding function to generate colors

--- day-18/random_walk.py
+++ day-18/random_walk.py
@@ -1,18 +1,25 @@
 import turtle as t
 import random
+from turtle import Screen
 
 tim = t.Turtle()
-tim.pensize(5)
+screen = Screen()
 
-steps = 1
+tim.pensize(8)
 directions = [0, 90, 180, 270]
+screen.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
 
 while True:
-    R = random.random()
-    B = random.random()
-    G = random.random()
-    tim.color(R, G, B)
-
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
