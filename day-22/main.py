0bba9f262304633ba34f6dc38f3c19d47cf4c5aa
Erick Silva <erick.ap.silva@gmail.com>
2023-12-31 17:53:05 -0300

    detect when the ball goes out of bounds

--- day-22/main.py
+++ day-22/main.py
@@ -26,13 +26,20 @@ while game_is_one:
     screen.update()
     ball.move()
 
-    #Detect collision with ball
+    # Detect collision with ball
     if ball.ycor() > 280 or ball.ycor() <-280:
         ball.bounce_y()
 
-    #Detect collision with paddle
+    # Detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
 
+    # Detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    # Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 screen.exitonclick()
