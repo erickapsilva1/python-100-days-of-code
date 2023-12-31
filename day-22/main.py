9b73ec4d6a1d43416fd32d0a858fdb694998cfd4
Erick Silva <erick.ap.silva@gmail.com>
2023-12-31 17:37:56 -0300

    detecting collisions with the paddle

--- day-22/main.py
+++ day-22/main.py
@@ -28,6 +28,11 @@ while game_is_one:
 
     #Detect collision with ball
     if ball.ycor() > 280 or ball.ycor() <-280:
-        ball.bounce()
+        ball.bounce_y()
+
+    #Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
 
 screen.exitonclick()
