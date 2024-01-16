4a1a1cc58c01381fec179cc4e43306fb2766cc7c
Erick Silva <erick.ap.silva@gmail.com>
2024-01-16 08:09:24 -0300

    Read and Write the High Score to a File in Snake

--- day-24/scoreboard.py
+++ day-24/scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.retrieve_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -21,7 +21,9 @@ class Scoreboard(Turtle):
 
     def reset(self):
         if self.score > self.high_score:
-            self.high_score = self.score
+            with open("data.txt", mode="w") as user_score:
+                user_score.write(str(self.score))
+            self.retrieve_score()
         self.score = 0
         self.update_scoreboard()
 
@@ -33,3 +35,7 @@ class Scoreboard(Turtle):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def retrieve_score(self):
+        with open("data.txt") as user_score:
+            self.high_score = int(user_score.read())
