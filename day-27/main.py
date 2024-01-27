1e6fbd4ad2aed4c2af128b5bd99c7e56550bd506
Erick Silva <erick.ap.silva@gmail.com>
2024-01-27 12:38:59 -0300

    button and entry

--- day-27/main.py
+++ day-27/main.py
@@ -1,4 +1,5 @@
 import tkinter
+from builtins import input
 
 window = tkinter.Tk()
 window.title("My First GUI Program")
@@ -7,5 +8,33 @@ window.minsize(width=500, height=300)
 # Label
 my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "New Text"
+my_label.config(text="New Text")
+
+# Button
+def button_clicked():
+    print("I got clicked")
+    my_label.config(text="Button Got Clicked")
+
+#button.pack()
+#button = tkinter.Button(text="Click Me", command=button_clicked)
+
+# Entry
+input = tkinter.Entry(width=10)
+
+
+def button_clicked2():
+    my_label.config(text=input.get())
+
+
+button = tkinter.Button(text="Click Me", command=button_clicked2)
+
+button.pack()
+input.pack()
+
+
+
+
 
 window.mainloop()
