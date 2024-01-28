f339ad0e25f907a484fbc4134129281939e8a96c
Erick Silva <erick.ap.silva@gmail.com>
2024-01-28 09:49:56 -0300

    a lot of components

--- day-27/main.py
+++ day-27/main.py
@@ -1,40 +1,102 @@
-import tkinter
-from builtins import input
+from tkinter import *
 
-window = tkinter.Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+# Creating a new window and configurations
+window = Tk()
+window.title("Widget Examples")
+window.minsize(width=500, height=500)
 
-# Label
-my_label = tkinter.Label(text="I am a label", font=("Arial", 24, "bold"))
-my_label.pack()
+# Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+label.pack()
 
-my_label["text"] = "New Text"
-my_label.config(text="New Text")
+# Buttons
+def action():
+    print("Do something")
 
-# Button
-def button_clicked():
-    print("I got clicked")
-    my_label.config(text="Button Got Clicked")
-
-#button.pack()
-#button = tkinter.Button(text="Click Me", command=button_clicked)
-
-# Entry
-input = tkinter.Entry(width=10)
-
-
-def button_clicked2():
-    my_label.config(text=input.get())
-
-
-button = tkinter.Button(text="Click Me", command=button_clicked2)
-
+# Calls action() when press button
+button = Button(text="Click me",
+                command=action)
 button.pack()
-input.pack()
 
+# Entries
+entry = Entry(width=30)
+# Add some text do begin with
+entry.insert(END, string="Some text to begin with.")
+# Gets text in entry
+print(entry.get())
+entry.pack()
 
+# Text
+text = Text(height=5, width=30)
+# Puts cursor in textbox
+text.focus()
+# Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry")
+# Gets current value in textbox at line 1, char 0
+print(text.get("1.0", END))
+text.pack()
 
+# Spinbox
+def spinbox_used():
+    # Gets the current value in spinbox
+    print(spinbox.get())
 
+spinbox = Spinbox(from_=0, to=10, width=5,
+                  command=spinbox_used)
+spinbox.pack()
+
+# Scale
+# Called with current scale value.
+def scale_used(value):
+    print(value)
+
+scale = Scale(from_=0, to=100,
+              command=scale_used)
+scale.pack()
+
+# Checkbutton
+def checkbutton_used():
+    # Prints 1 if On button checked,
+    # otherwise 0
+    print(check_state.get())
+
+# Variable to hold on to checked state, 0
+# is off, 1 is on.
+check_state = IntVar()
+check_button = Checkbutton(text="Is on?",
+                           variable=check_state,
+                           command=checkbutton_used)
+check_state.get()
+check_button.pack()
+
+# Radiobutton
+def radio_used():
+    print(radio_state.get())
+# Variable to hold on to which radio
+# button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option 1",
+                           value=1, variable=radio_state,
+                           command=radio_used)
+radiobutton2 = Radiobutton(text="Option 2",
+                           value=2, variable=radio_state,
+                           command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+# Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item),
+                   item)
+listbox.bind("<<ListboxSelect>>",
+             listbox_used)
+listbox.pack()
 
 window.mainloop()
