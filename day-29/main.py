44976df5ff39a7adf81c09eb6b28860ee58ac998
Erick Silva <erick.ap.silva@gmail.com>
2024-01-30 21:06:15 -0300

    Password Manager project - adding components

--- day-29/main.py
+++ day-29/main.py
@@ -8,11 +8,40 @@ window = Tk()
 window.title("Password Manager")
 window.config(padx=20, pady=20)
 
-canvas = Canvas(width=200, height=200, highlightthickness=0)
+canvas = Canvas(height=200, width=200)
 lock_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=lock_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+def generate_password():
+    pass
+
+def add():
+    pass
 
 
+label_website = Label(text="Website:")
+label_website.grid(column=0, row=1)
+
+entry_website = Entry()
+entry_website.grid(column=1, row=1, columnspan=2, sticky="EW")
+
+label_email_uname = Label(text="Email/Username:")
+label_email_uname.grid(column=0, row=2)
+
+entry_email_uname = Entry()
+entry_email_uname.grid(column=1, row=2, columnspan=2, sticky="EW")
+
+label_password = Label(text="Password:")
+label_password.grid(column=0, row=3)
+
+entry_password = Entry()
+entry_password.grid(column=1, row=3, sticky="EW")
+
+generate_btn = Button(text="Generate Password")
+generate_btn.grid(column=2, row=3, sticky="EW")
+
+add_btn = Button(text="Add", width=35)
+add_btn.grid(column=1, row=4, columnspan=2, sticky="EW")
 
 window.mainloop()
