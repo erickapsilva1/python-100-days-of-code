f011a8c6710e5bf8633adb04258f577c845cad9f
Erick Silva <erick.ap.silva@gmail.com>
2024-02-02 07:34:31 -0300

    Password Manager project - passwd generator & copy to clipboard feat.

--- day-29/main.py
+++ day-29/main.py
@@ -1,6 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
+import random
+import pyperclip
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def generate_passwd():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    passwd_letters = [random.choice(letters) for _ in range(nr_letters)]
+    passwd_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    passwd_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    passwd_list = passwd_letters + passwd_symbols + passwd_numbers
+
+    random.shuffle(passwd_list)
+
+    password = "".join(passwd_list)
+
+    entry_password.delete(0, END)
+    entry_password.insert(0, string=password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def add():
@@ -21,6 +46,7 @@ def add():
 
     else:
         messagebox.showinfo(title="Ops", message="Please don't leave any field empty!")
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -56,7 +82,7 @@ label_password.grid(column=0, row=3)
 entry_password = Entry()
 entry_password.grid(column=1, row=3, sticky="EW")
 
-generate_btn = Button(text="Generate Password")
+generate_btn = Button(text="Generate Password", command=generate_passwd)
 generate_btn.grid(column=2, row=3, sticky="EW")
 
 add_btn = Button(text="Add", width=35, command=add)
